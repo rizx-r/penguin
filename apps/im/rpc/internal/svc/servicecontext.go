d923badadfc16b4eed3f70e601d86b0413271db5
Rizx <kinalvate@163.com>
2025-09-03 17:47:25 +0800

    service context

--- apps/im/rpc/internal/svc/servicecontext.go
+++ apps/im/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,23 @@
 package svc
 
-import "penguin/apps/im/rpc/internal/config"
+import (
+	"penguin/apps/im/im_models"
+	"penguin/apps/im/rpc/internal/config"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	im_models.ChatLogModel
+	im_models.ConversationsModel
+	im_models.ConversationModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:             c,
+		ChatLogModel:       im_models.MustChatLogModel(c.Mongo.Url, c.Mongo.Db),
+		ConversationsModel: im_models.MustConversationsModel(c.Mongo.Url, c.Mongo.Db),
+		ConversationModel:  im_models.MustConversationModel(c.Mongo.Url, c.Mongo.Db),
 	}
 }
