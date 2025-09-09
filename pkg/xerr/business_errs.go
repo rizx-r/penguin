f43908d0280ff7c4e8ed6b5e833d6149c7993d67
Rizx <kinalvate@163.com>
2025-09-09 14:52:12 +0800

    业务异常

--- pkg/xerr/business_errs.go
+++ pkg/xerr/business_errs.go
@@ -3,9 +3,17 @@ package xerr
 import "errors"
 
 var (
-	ErrPhoneNotRegistered          = errors.New("phone not registered")
-	ErrPhoneRegistered             = errors.New("phone registered")
-	ErrUserPwdNotMatched           = errors.New("user password not matched")
+	// 用户注册
+	ErrPhoneNotRegistered = errors.New("phone not registered")
+	ErrPhoneRegistered    = errors.New("phone registered")
+	ErrUserPwdNotMatched  = errors.New("user password not matched")
+
+	// 好友申请
 	ErrFriendRequestAlreadyPassed  = NewMsg("friend request already passed")
 	ErrFriendRequestAlreadyRefused = NewMsg("friend request already refused")
+
+	// 群申请
+	ErrGroupRequestAlreadyExists  = NewMsg("group request already exists")
+	ErrGroupRequestAlreadyPassed  = NewMsg("group request already passed")
+	ErrGroupRequestAlreadyRefused = NewMsg("group request already refused")
 )
