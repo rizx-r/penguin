7bde71028bf7a612e0e1b987575b43accb202972
Rizx <kinalvate@163.com>
2025-09-10 10:47:15 +0800

    公共代码

--- pkg/constants/social.go
+++ pkg/constants/social.go
@@ -1,11 +1,31 @@
 package constants
 
-// 处理结果  1未处理，2.处理，3.拒绝
-type HandlerResult int
+type (
+	// 处理结果  1未处理，2.处理，3.拒绝
+	HandlerResult int
 
+	// 群成员等级 1. 群主 2. 管理员，3. 普通群成员
+	GroupRoleLevel int
+
+	// 进群申请的方式： 1. 邀请， 2. 申请
+	GroupJoinSource int
+)
+
+// HandlerResult
 const (
 	NoHandleResult HandlerResult = iota + 1
 	PassHandleResult
 	RefuseHandleResult
 	CancelHandleResult
+)
+
+const (
+	MasterGroupRoleLevel GroupRoleLevel = iota + 1
+	ManagerGroupRoleLevel
+	OrdinaryGroupRoleLevel
+)
+
+const (
+	InviteGroupJoinSource GroupJoinSource = iota + 1
+	PutInGroupJoinSource
 )
--- pkg/wuid/wuid.go
+++ pkg/wuid/wuid.go
@@ -1,8 +1,3 @@
-/**
- * @author: dn-jinmin/dn-jinmin
- * @doc:
- */
-
 package wuid
 
 import (
@@ -13,10 +8,13 @@ import (
 	"strconv"
 )
 
+// 全局的 WUID 实例，用来生成唯一 ID
 var w *wuid.WUID
 
+// Init /*
+// dns: 数据库连接字符串
 func Init(dsn string) {
-
+	// newDB: 用来创建 MySQL 连接的函数
 	newDB := func() (*sql.DB, bool, error) {
 		db, err := sql.Open("mysql", dsn)
 		if err != nil {
@@ -25,16 +23,18 @@ func Init(dsn string) {
 		return db, true, nil
 	}
 
-	w = wuid.NewWUID("default", nil)
+	w = wuid.NewWUID("default", nil) // 创建一个新的 WUID 生成器。
+	// 从 MySQL 的 wuid 表里加载 Worker ID（类似雪花算法中的机器号），保证分布式环境下不会重复。
 	_ = w.LoadH28FromMysql(newDB, "wuid")
 }
 
+// GenUid 生成唯一 ID
 func GenUid(dsn string) string {
 	if w == nil {
 		Init(dsn)
 	}
 
-	return fmt.Sprintf("%#016x", w.Next())
+	return fmt.Sprintf("%#016x", w.Next()) // 把整数转成 固定长度的 16 位十六进制字符串（带 0x 前缀）。例如：0x00000017f6a3b12d
 }
 
 func CombineId(aid, bid string) string {
@@ -46,4 +46,9 @@ func CombineId(aid, bid string) string {
 	})
 
 	return fmt.Sprintf("%s_%s", ids[0], ids[1])
+	/*
+		用户 A 的 ID：0x0000000000000001
+		用户 B 的 ID：0x0000000000000002
+		CombineId 结果：0x0000000000000001_0x0000000000000002
+	*/
 }
