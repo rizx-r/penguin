4b30c872bfcd91d15c8ddda5ae147d24afeada34
Rizx <kinalvate@163.com>
2025-09-15 23:12:54 +0800

    实现群聊消息已读未读

--- apps/task/mq/mq/mq.go
+++ apps/task/mq/mq/mq.go
@@ -6,10 +6,20 @@ type (
 	MsgChatTransfer struct {
 		ConversationId     string `json:"conversationId"`
 		constants.ChatType `json:"chatType"`
-		SendId             string `json:"sendId"`
-		RecvId             string `json:"recvId"`
-		SendTime           int64  `json:"sendTime"`
+		SendId             string   `json:"sendId"`
+		RecvId             string   `json:"recvId"`
+		RecvIds            []string `json:"recvIds"`
+		SendTime           int64    `json:"sendTime"`
 		constants.MType    `json:"mType"`
 		Content            string `json:"content"`
+	}
+
+	// MsgMarkRead 处理已读的消费者
+	MsgMarkRead struct {
+		constants.ChatType `json:",chatType"`
+		ConversationId     string   `json:"conversationId"` // 会话id
+		SendId             string   `json:"sendId"`         // 发送者id
+		RecvId             string   `json:"recvId"`         // 接受者id
+		MsgIds             []string `json:"msgIds"`         // 消息id
 	}
 )
