c1ae5d18d930c41e6d1ebac5074c5fae8a6ac709
Rizx <kinalvate@163.com>
2025-09-19 12:20:06 +0800

    实现群聊消息已读未读

--- pkg/constants/im.go
+++ pkg/constants/im.go
@@ -1,12 +1,21 @@
 package constants
 
 type (
-	MType    int
-	ChatType int
+	MType       int
+	ChatType    int
+	ContentType int // 内容类型
 )
 
 const (
 	TextMtype              = iota // iota 表示 从 0 开始，每行递增 1 的常量生成器。
 	GroupChatType ChatType = iota
 	SingleChatType
+)
+
+// ContentType
+const (
+	// ContentChatMsg 消息聊天
+	ContentChatMsg ContentType = iota
+	// ContentMarkRead 记录已读
+	ContentMarkRead
 )
