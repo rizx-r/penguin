61cdb411d4da768de48ca84e8745ddbbb3838e4e
Rizx <kinalvate@163.com>
2025-09-19 14:19:48 +0800

    实现群聊消息已读未读

--- apps/im/rpc/internal/logic/getchatloglogic.go
+++ apps/im/rpc/internal/logic/getchatloglogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"github.com/pkg/errors"
+	//"go/types"
+	"penguin/pkg/xerr"
 
 	"penguin/apps/im/rpc/im"
 	"penguin/apps/im/rpc/internal/svc"
@@ -27,5 +30,45 @@ func NewGetChatLogLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetCha
 func (l *GetChatLogLogic) GetChatLog(in *im.GetChatLogReq) (*im.GetChatLogResp, error) {
 	// todo: add your logic here and delete this line
 
-	return &im.GetChatLogResp{}, nil
+	// 根据id
+
+	if in.MsgId != "" {
+		chatLog, err := l.svcCtx.ChatLogModel.FindOne(l.ctx, in.MsgId)
+		if err != nil {
+			return nil, errors.Wrapf(xerr.NewDBErr(), "find chat log by msgId error: %v, msgId: %s", err, in.MsgId)
+		}
+		return &im.GetChatLogResp{List: []*im.ChatLog{
+			{
+				Id:             chatLog.ID.Hex(), // 转成string
+				ConversationId: chatLog.ConversationID,
+				SendId:         chatLog.SendID,
+				RecvId:         chatLog.RecvID,
+				MsgType:        int32(chatLog.MsgType),
+				MsgContent:     chatLog.MsgContent,
+				ChatType:       int32(chatLog.ChatType),
+				SendTime:       chatLog.SendTime,
+				ReadRecords:    chatLog.ReadRecords,
+			}}}, nil
+	}
+
+	// 时间分段查询
+	data, err := l.svcCtx.ChatLogModel.ListBySendTime(l.ctx, in.ConversationId, in.StartSendTime, in.EndSendTime, in.Count)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewDBErr(), "list chat log by sendTime error: %v, req: %v", err, in)
+	}
+	res := make([]*im.ChatLog, 0, len(data))
+	for _, d := range data {
+		res = append(res, &im.ChatLog{
+			Id:             d.ID.Hex(),
+			ConversationId: d.ConversationID,
+			SendId:         d.SendID,
+			RecvId:         d.RecvID,
+			MsgType:        int32(d.MsgType),
+			MsgContent:     d.MsgContent,
+			ChatType:       int32(d.ChatType),
+			SendTime:       d.SendTime,
+			ReadRecords:    d.ReadRecords,
+		})
+	}
+	return &im.GetChatLogResp{List: res}, nil
 }
