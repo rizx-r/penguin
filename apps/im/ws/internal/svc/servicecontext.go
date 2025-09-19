2bb3d06cdc71323aafb27d7f82990c2f6ed9b8f4
Rizx <kinalvate@163.com>
2025-09-19 12:35:28 +0800

    实现群聊消息已读未读

--- apps/im/ws/internal/svc/servicecontext.go
+++ apps/im/ws/internal/svc/servicecontext.go
@@ -9,15 +9,17 @@ import (
 type (
 	ServiceContext struct {
 		Config config.Config
-		mqclient.MsgChatTransferClient
 		im_models.ChatLogModel
+		mqclient.MsgChatTransferClient
+		mqclient.MsgReadTransferClient
 	}
 )
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:                c,
-		MsgChatTransferClient: mqclient.NewMsgChatTransferClient(c.MsgChatTransfer.Addrs, c.MsgChatTransfer.Topic),
 		ChatLogModel:          im_models.MustChatLogModel(c.Mongo.Url, c.Mongo.Db),
+		MsgChatTransferClient: mqclient.NewMsgChatTransferClient(c.MsgChatTransfer.Addrs, c.MsgChatTransfer.Topic),
+		MsgReadTransferClient: mqclient.NewMsgReadTransferClient(c.MsgReadTransfer.Addrs, c.MsgReadTransfer.Topic),
 	}
 }
